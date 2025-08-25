54c0f5d93bd8537ac08becbd18819b41886eb98b
Ramiro Gestoso <rgestoso@fi.uba.ar>
2025-08-25 19:47:58 -0300

    add: generar compose script

---
+++ generar-compose.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+FILENAME=$1
+CLIENTS_COUNT=$2
+
+function generate_clients {
+    local clients=""
+    for i in $(seq 1 $1); do
+      clients+="
+  client$i:
+    container_name: client$i
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID=$i
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+"
+    done
+    echo "$clients"
+}
+
+DOCKER_COMPOSE="name: tp0
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+$(generate_clients $CLIENTS_COUNT)
+
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24"
+
+echo "$DOCKER_COMPOSE" > "$FILENAME"
