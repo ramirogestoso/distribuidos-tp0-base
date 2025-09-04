070d3b0d23a9f90fde6ab7146c711a2cd954927b
Ramiro Gestoso <rgestoso@fi.uba.ar>
2025-09-03 21:39:04 -0300

    use select to listen to multiple sockets - when a socket is ready, then handles it (read batch)

--- server/common/server.py
+++ server/common/server.py
@@ -1,5 +1,6 @@
 import socket
 import logging
+from select import select
 
 from protocol.message import BatchMessage, CodeMessage, LotteryResultMessage
 from common.utils import has_won, load_bets, store_bets
@@ -7,13 +8,17 @@ from common.utils import has_won, load_bets, store_bets
 class Server:
     def __init__(self, port, listen_backlog, clients_count):
         # Initialize server socket
+        self._sockets = []
+        self.__initialize_server_socket(port, listen_backlog)
+        self._running = True
+        self._clients_count = clients_count
+        self._waiting_clients_sockets = dict()  # Map of agency to client socket
+
+    def __initialize_server_socket(self, port, listen_backlog):
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
-        self._current_client_socket = None
-        self._running = True
-        self._clients_count = clients_count
-        self._clients_sockets = dict()  # Map of agency to client socket
+        self._sockets.append(self._server_socket)
 
     def run(self):
         """
@@ -26,30 +31,41 @@ class Server:
 
         while self._running:
             try:
-                if len(self._clients_sockets) >= self._clients_count:
+                if len(self._waiting_clients_sockets) >= self._clients_count:
                     self.__lottery()
-                self._current_client_socket = self.__accept_new_connection()
-                self.__handle_client_connection(self._current_client_socket)
+ 
+                read_sockets, _, exception_sockets = select(self._sockets, [], self._sockets)
+                for sock in read_sockets:
+                    if sock == self._server_socket: self.__accept_new_connection()
+                    else: self.__handle_client_connection(sock)
+                
+                for sock in exception_sockets:
+                    self.__close_client_socket(sock)
+
             except OSError:
                if not self._running: break
                raise
-    
+
     def __lottery(self):
         winnerBets = [bet for bet in load_bets() if has_won(bet)]
         winners_by_agency = dict()
-        for agency in self._clients_sockets.keys():
+        for agency in self._waiting_clients_sockets.keys():
             winners_by_agency[agency] = set()
         for bet in winnerBets:
             winners_by_agency[bet.agency].add(bet.document)
 
         for agency, documents in winners_by_agency.items():
-            client_socket = self._clients_sockets[agency]
+            client_socket = self._waiting_clients_sockets[agency]
             try: LotteryResultMessage(documents).write_to(client_socket)
-            finally: client_socket.close()
+            finally: self.__close_client_socket(client_socket)
 
-        self._clients_sockets.clear()
+        self._waiting_clients_sockets.clear()
 
         logging.info("action: sorteo | result: success")
+
+    def __close_client_socket(self, client_sock):
+        client_sock.close()
+        if client_sock in self._sockets: self._sockets.remove(client_sock)
 
     def __handle_client_connection(self, client_sock):
         """
@@ -59,18 +75,18 @@ class Server:
         client socket will also be closed
         """
         bets = []
-        while True:
-            try:
-                bets, agency = BatchMessage.read_bets(client_sock)
-                self._clients_sockets[int(agency)] = client_sock
-                if not bets:
-                    break
-                store_bets(bets)
-                logging.info(f'action: apuesta_recibida | result: success | cantidad: {len(bets)} | agency: {agency}')
-                CodeMessage(len(bets)).write_to(client_sock)
-            except OSError as e:
-                logging.error(f"action: apuesta_recibida | result: fail | cantidad: {len(bets)} | error: {e}")
-                CodeMessage(0).write_to(client_sock)
+        try:
+            bets, agency = BatchMessage.read_bets(client_sock)
+            if not bets:
+                self._waiting_clients_sockets[int(agency)] = client_sock
+                return
+            # delegate this to an async queue
+            store_bets(bets)
+            logging.info(f'action: apuesta_recibida | result: success | cantidad: {len(bets)} | agency: {agency}')
+            CodeMessage(len(bets)).write_to(client_sock)
+        except OSError as e:
+            logging.error(f"action: apuesta_recibida | result: fail | cantidad: {len(bets)} | error: {e}")
+            CodeMessage(0).write_to(client_sock)
 
     def __accept_new_connection(self):
         """
@@ -83,6 +99,7 @@ class Server:
         # Connection arrived
         logging.info('action: accept_connections | result: in_progress')
         c, addr = self._server_socket.accept()
+        self._sockets.append(c)
         logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
         return c
 
@@ -92,7 +109,5 @@ class Server:
         """
         self._running = False
         self._server_socket.close()
-        if self._current_client_socket:
-            self._current_client_socket.close()
-        for sock in self._clients_sockets.values():
+        for sock in self._sockets:
             sock.close()
